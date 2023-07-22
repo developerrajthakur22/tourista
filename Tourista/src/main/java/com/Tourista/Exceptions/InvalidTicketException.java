f410064911eba9ebf5770ac965893aa4356bf6bc
Rutwik Kumbhar <kumbharrutwik@gmail.com>
2023-07-22 09:31:21 +0530

    form and packages file added

--- Tourista/src/main/java/com/Tourista/Exceptions/InvalidTicketException.java
+++ Tourista/src/main/java/com/Tourista/Exceptions/InvalidTicketException.java
@@ -1,5 +1,11 @@
 package com.Tourista.Exceptions;
 
 public class InvalidTicketException extends Exception {
-
+	public InvalidTicketException() {
+		super(); 
+	}
+	
+	public InvalidTicketException(String message) {
+		super(message); 
+	}
 }
