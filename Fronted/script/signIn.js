0295101e6eeea9f52bf22b1122b600d9d612b9ec
unknown <harshalmusmade410@gmail.com>
2023-07-23 23:04:59 +0530

    updated signIn Harshal

--- Fronted/script/signIn.js
+++ Fronted/script/signIn.js
@@ -1,43 +1,27 @@
 function logIn(){
+
     let username = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
-<<<<<<< HEAD
-    fetch(`http://localhost:8888/userLogin/${username}/${password}`)
-    .then(data=>{
-        return data.json();
-=======
-    fetch("http://localhost:8888/userLogin",{
-        method:"GET",
-        headers:{
-            'Authorization' : 'Basic' + btoa(username + ":" + password)
-        }
-    }).then(response => {
-        if(response.status == 201 | response.status == 200){
-            const token = response.headers.get("Authorization");
-            console.log(token);
-            localStorage.setItem("jwtToken",token);
-
-            response.json().then(data =>{
-                localStorage.setItem("username",data.name);
-            })
-
-            const url = "/Frontend/signIn.html";
-
-            location.href = url;
-        }
-        else{
-            alert("Invalid username or password");
-        }
->>>>>>> 335329df220e108bfbc7dc46840509f2133f73e1
-    })
-    .then(data=>{
-       // console.log(data);   
-        localStorage.setItem("customer", JSON.stringify(data));
-        localStorage.setItem("customerId", JSON.stringify(data.customer_id));
-        const url = "../index.html";
+    if(username == "admin" && password == "admin"){
+        const url = "/Fronted/Admin/admin.html";
         location.href = url;
-    })
+    }
+    else{
+        fetch(`http://localhost:8888/userLogin/${username}/${password}`)
+        .then(data=>{
+            return data.json();
+        })
+        .then(data=>{
+           // console.log(data);   
+            localStorage.setItem("customer", JSON.stringify(data));
+            localStorage.setItem("customerId", JSON.stringify(data.customer_id));
+            const url = "/Fronted/index.html";
+            location.href = url;
+        })
+
+    }
+
 
 
     // fetch("http://localhost:8888/userLogin",{
--- Touristo/src/main/java/com/Touristo/Controller/AdminController.java
+++ Touristo/src/main/java/com/Touristo/Controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Touristo.Entity.Bus;
 import com.Touristo.Entity.Hotel;
 import com.Touristo.Entity.Packages;
+import com.Touristo.Exception.NotFoundException;
 import com.Touristo.Service.AdminServices;
 
 @RestController
