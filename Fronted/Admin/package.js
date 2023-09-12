e9378c86226f116fc9c24afd7e17dbfc6464cbe4
unknown <harshalmusmade410@gmail.com>
2023-09-12 18:13:18 +0530

    Lpush

--- Fronted/Admin/package.js
+++ Fronted/Admin/package.js
@@ -17,7 +17,7 @@ function addPackage(){
         })
     }).then(response => {
         if(response.status == 201 | response.status == 200 | response.status == 202){
-            response.json().then(data => alert("Hotel added successful"))
+            response.json().then(data => alert("Package added successful"))
         }else{
             response.json().then(data => alert(data.message));
         }
